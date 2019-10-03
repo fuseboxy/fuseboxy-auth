9cf2378567bad0078d84d528cbe274dbd11902fc
Henry Shum <henry.shum@cityu.edu.hk>
2019-10-03 15:40:37 +0800

    display number of users of each role at tab layout

--- app/view/user/layout.php
+++ app/view/user/layout.php
@@ -1,22 +1,27 @@
 <?php
-// tab layout config
+// config
 $tabLayout = array(
 	'style' => 'tab',
 	'position' => 'left',
 	'header' => '<h3>User</h3>',
-	'nav' => array(),
+	'nav' => call_user_func(function(){
+		$menus = array();
+		// all existing roles
+		$roles = R::getCol('SELECT DISTINCT role FROM user WHERE role != ? ORDER BY role ASC', array('SUPER'));
+		if ( Auth::activeUserInRole('SUPER') ) array_unshift($roles, 'SUPER');
+		// put into result
+		foreach ( $roles as $item ) {
+			$menus[] = array(
+				'name' => ucwords( strtolower( $item ) ),
+				'url' => F::url( F::command('controller').'&role='.$item ),
+				'active' => ( isset($_SESSION['userController__userRole']) and $_SESSION['userController__userRole'] == $item ),
+				'remark' => R::count('user', 'role = ? AND disabled = 0', array($item)),
+			);
+		}
+		// done!
+		return $menus;
+	}),
 );
-
-
-// tab : all existing types
-$roles = R::getCol("SELECT DISTINCT role FROM user " . ( !Auth::activeUserInRole('SUPER') ? " WHERE role != 'SUPER' " : "" ) );
-foreach ( $roles as $t ) {
-	$tabLayout['nav'][] = array(
-		'name' => ucwords( strtolower( $t ) ),
-		'url' => F::url("{$fusebox->controller}&role={$t}"),
-		'active' => ( isset($_SESSION['user_role']) and $_SESSION['user_role'] == $t )
-	);
-}
 
 
 // tab layout
@@ -25,5 +30,5 @@ include F::config('appPath').'view/global/tab.php';
 $layout['content'] = ob_get_clean();
 
 
-// wrap by global layout
+// global layout
 include F::config('appPath').'view/global/layout.php';
