706bf61f212e727efdcfa4fcdad2175cdf293db1
Henry Shum <henry.shum@cityu.edu.hk>
2019-10-03 15:51:08 +0800

    apply namespace to session variable

--- app/controller/user_controller.php
+++ app/controller/user_controller.php
@@ -4,14 +4,14 @@ F::redirect(F::config('defaultCommand'), !Auth::activeUserInRole('SUPER,ADMIN'))
 
 
 // default role
-if ( !isset($_SESSION['user_role']) ) {
-	$_SESSION['user_role'] = R::getCell("SELECT role FROM user ORDER BY role ");
+if ( !isset($_SESSION['userController__userRole']) ) {
+	$_SESSION['userController__userRole'] = R::getCell("SELECT role FROM user ORDER BY role ");
 }
 
 
 // change selected role (pass by url from layout)
 if ( isset($arguments['role']) ) {
-	$_SESSION['user_role'] = $arguments['role'];
+	$_SESSION['userController__userRole'] = $arguments['role'];
 }
 
 
@@ -21,7 +21,7 @@ $scaffold = array(
 	'editMode' => 'inline',
 	'allowDelete' => Auth::activeUserInRole('SUPER'),
 	'layoutPath' => F::config('appPath').'view/user/layout.php',
-	'listFilter' => array('role = ?', array($_SESSION['user_role'])),
+	'listFilter' => array('role = ?', array($_SESSION['userController__userRole'])),
 	'listOrder' => 'ORDER BY username',
 	'listField' => array(
 		'id' => '5%',
@@ -31,12 +31,12 @@ $scaffold = array(
 	),
 	'fieldConfig' => array(
 		'id' => array(),
-		'username' => array('placeholder' => 'Login'),
+		'username' => array('placeholder' => 'Username'),
 		'password' => array('placeholder' => 'Password'),
-		'role' => array('default' => $_SESSION['user_role'], 'readonly' => !Auth::activeUserInRole('SUPER')),
-		'fullname' => array('placeholder' => 'Full Name'),
+		'role' => array('default' => $_SESSION['userController__userRole'], 'readonly' => !Auth::activeUserInRole('SUPER')),
+		'fullname' => array('label' => 'Full Name', 'placeholder' => 'Full Name'),
 		'email' => array('placeholder' => 'Email'),
-		'tel' => array('placeholder' => 'Phone')
+		'tel' => array('placeholder' => 'Tel')
 	),
 	'writeLog' => true,
 );
