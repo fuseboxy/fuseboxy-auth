92625f9703b6f0d26aa4377232f298f28cca51a2
Henry Shum <henry.shum@cityu.edu.hk>
2019-10-03 14:27:47 +0800

    minor change in wording

--- app/model/Auth.php
+++ app/model/Auth.php
@@ -84,7 +84,7 @@ class Auth {
 		if ( empty($user->id) ) $userByEmail = R::findOne('user', 'email = ? ', array($data['username']));
 		// check user existence
 		if ( empty($user->id) and empty($userByEmail->id) ) {
-			self::$error = "User not found (username={$data['username']})";
+			self::$error = "User account <strong><em>{$data['username']}</em></strong> not found";
 			return false;
 		}
 		// check user status
