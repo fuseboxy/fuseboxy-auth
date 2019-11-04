f034cf0e8b806cc75b4359ba4890efe5c49123a6
henrygotmojo <henrygotmojo@gmail.com>
2019-11-04 18:57:54 +0800

    minor spacing adjustment

--- app/view/auth/index.php
+++ app/view/auth/index.php
@@ -13,15 +13,9 @@
 */ ?>
 <div id="auth-index" class="px-2 pt-2 pb-0">
 	<div class="form-group">
-		<a 
-			href="<?php echo F::url($xfa['cas']); ?>" 
-			class="btn w-100 py-3 btn-primary"
-		>CAS Login</a>
+		<a href="<?php echo F::url($xfa['cas']); ?>" class="btn w-100 py-3 btn-primary">CAS Login</a>
 	</div>
 	<div class="form-group">
-		<a 
-			href="<?php echo F::url($xfa['local']); ?>" 
-			class="btn w-100 py-3 btn-outline-primary text-primary bg-white"
-		>Local Account Login</a>
+		<a href="<?php echo F::url($xfa['local']); ?>" class="btn w-100 py-3 btn-outline-primary text-primary bg-white">Local Account Login</a>
 	</div>
 </div>
