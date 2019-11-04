07858bff3b57361f0c41ef51278ef676ccabf186
henrygotmojo <henrygotmojo@gmail.com>
2019-11-04 18:59:52 +0800

    adjust auth panel structure

--- app/view/auth/panel.php
+++ app/view/auth/panel.php
@@ -25,12 +25,10 @@
 <div id="auth-panel" class="container" style="margin-top: 50vh; transform: translateY(-60%);">
 	<div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-5 offset-lg-3">
 		<div class="card panel-default">
-			<div class="card-header text-center">
-				<a href="<?php echo F::url($xfa['auth']); ?>" class="btn">
-					<h3><?php echo $layout['panelTitle']; ?></h3>
-					<h5 class="text-muted"><?php echo $layout['panelSubtitle']; ?></h5>
-				</a>
-			</div>
+			<a href="<?php echo F::url($xfa['auth']); ?>" class="card-header btn">
+				<h3><?php echo $layout['panelTitle']; ?></h3>
+				<h5 class="text-muted"><?php echo $layout['panelSubtitle']; ?></h5>
+			</a>
 			<div class="card-body"><?php
 				if ( isset($authLayout['flash']) ) echo $authLayout['flash'];
 				if ( isset($layout['content']) ) echo $layout['content'];
