64761fa64de0e6f1b326ce3d0de80e8e2769223f
henrygotmojo <henrygotmojo@gmail.com>
2020-01-16 18:26:14 +0800

    finish sim-user list in modal

--- app/view/sim/index.php
+++ app/view/sim/index.php
@@ -4,13 +4,17 @@
 		<in>
 			<structure name="$xfa">
 				<string name="start" />
+				<string name="end" />
 			</structure>
 			<structure name="$users">
-				<object name="~id~">
-					<number name="id" />
-					<string name="username" />
-					<string name="fullname" />
-				</object>
+				<structure name="~role~">
+					<object name="~id~">
+						<number name="id" />
+						<string name="role" />
+						<string name="username" />
+						<string name="fullname" />
+					</object>
+				</structure>
 			</structure>
 		</in>
 		<out>
@@ -20,16 +24,33 @@
 </fusedoc>
 */ ?>
 <div id="sim-index">
-	<ul>
-		<?php foreach ( $users as $id => $item ) : ?>
-			<li>
-				<?php if ( !empty($item['fullname']) ) : ?>
-					<em class="small text-muted float-right"><?php echo $item['fullname']; ?></em>
-				<?php endif; ?>
-				<a href="<?php echo F::url("{$xfa['start']}&user_id={$id}"); ?>">
-					<?php echo $item['username']; ?>
-				</a>
-			</li>
+	<div class="modal-header">
+		<div class="modal-title h5">User Simulation</div>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	</div>
+	<div class="modal-body">
+		<?php foreach ( $users as $role => $arr ) : ?>
+			<?php if ( $role != array_keys($users)[0] ) : ?>
+				<hr class="row" />
+			<?php endif; ?>
+			<div class="px-3 pt-2 pb-2">
+				<h6 class="mb-3 font-weight-normal <?php echo in_array(Sim::user('id'), array_keys($arr)) ? 'text-primary' : 'text-muted'; ?>">
+					<?php echo $role; ?>
+				</h6>
+				<?php foreach ( $arr as $id => $item ) : ?>
+					<a
+						class="badge <?php echo ( Sim::user('id') == $id ) ? 'badge-primary' : 'badge-light'; ?>"
+						href="<?php echo F::url($xfa['start'].'&user_id='.$id); ?>"
+					><?php echo $item->username; ?></a>
+				<?php endforeach; ?>
+			</div>
 		<?php endforeach; ?>
-	</ul>
+	</div>
+	<div class="modal-footer">
+		<?php if ( isset($xfa['end']) ) : ?>
+			<a href="<?php echo F::url($xfa['end']); ?>" class="btn btn-light"><i class="fa fa-ban mr-1"></i> End Sim</a>
+		<?php else : ?>
+			<button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+		<?php endif; ?>
+	</div>
 </div>
