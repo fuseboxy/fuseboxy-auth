49fbb29bcb7471d21424c6bf24fb509b73be6557
henrygotmojo <henrygotmojo@gmail.com>
2020-06-05 10:57:56 +0800

    fix user-layout by apply ORM component and deprecate direct use of RedBeanPHP

--- app/view/user/layout.php
+++ app/view/user/layout.php
@@ -7,7 +7,9 @@ $tabLayout = array(
 	'nav' => call_user_func(function(){
 		$menus = array();
 		// all existing roles
-		$roles = R::getCol('SELECT DISTINCT role FROM user WHERE role != ? ORDER BY role ASC', array('SUPER'));
+		$roles = array_map(function($item){
+			return $item['role'];
+		}, ORM::query('SELECT DISTINCT role FROM user WHERE role != ? ORDER BY role ASC', array('SUPER')));
 		if ( Auth::activeUserInRole('SUPER') ) array_unshift($roles, 'SUPER');
 		// put into result
 		foreach ( $roles as $item ) {
@@ -15,7 +17,7 @@ $tabLayout = array(
 				'name' => ucwords( strtolower( $item ) ),
 				'url' => F::url( F::command('controller').'&role='.$item ),
 				'active' => ( isset($_SESSION['userController__userRole']) and $_SESSION['userController__userRole'] == $item ),
-				'remark' => R::count('user', 'role = ? AND disabled = 0', array($item)),
+				'remark' => ORM::count('user', 'role = ? AND disabled = 0', array($item)),
 			);
 		}
 		// done!
