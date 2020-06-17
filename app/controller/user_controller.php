1da950a2e41a440dd878e7b90046b0280d21abdb
henrygotmojo <henrygotmojo@gmail.com>
2020-06-17 11:20:46 +0800

    minor change in wording

--- app/controller/user_controller.php
+++ app/controller/user_controller.php
@@ -1,6 +1,6 @@
 <?php
 F::redirect('auth', !Auth::user());
-F::error('Access Denied', !Auth::activeUserInRole('SUPER,ADMIN'));
+F::error('Forbidden', !Auth::activeUserInRole('SUPER,ADMIN'));
 
 
 // default role & retain selected role
