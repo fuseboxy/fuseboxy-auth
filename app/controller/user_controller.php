28d4c31bde7aa478d58f66a963dc38bf12c274fd
henrygotmojo <henrygotmojo@gmail.com>
2021-08-10 14:46:35 +0800

    assign random password when create user with empty password

--- app/controller/user_controller.php
+++ app/controller/user_controller.php
@@ -13,28 +13,36 @@ if ( $_SESSION['userController__userRole'] == 'SUPER' and Auth::user('role') != 
 
 
 // run!
-switch ( $fusebox->action ) {
+switch ( $fusebox->action ) :
 
 
 	// data manipulation before save
 	case 'save':
+		if ( isset($arguments['data']['password'])     ) $arguments['data']['password']     = trim($arguments['data']['password']);
+		if ( isset($arguments['data']['new_password']) ) $arguments['data']['new_password'] = trim($arguments['data']['new_password']);
 		// do not allow empty password (when create user)
-		if ( isset($arguments['data']['password']) and trim($arguments['data']['password']) === '' ) {
+		if ( isset($arguments['data']['password']) and $arguments['data']['password'] === '' ) {
 			$arguments['data']['password'] = Auth::generateRandomPassword(64);
 			F::error(Auth::error(), $arguments['data']['password'] === false);
 		}
 		// change password (when specified)
-		if ( !empty($arguments['data']['new_password']) ) $arguments['data']['password'] = $arguments['new_password'];
+		if ( isset($arguments['data']['new_password']) and strlen($arguments['data']['new_password']) ) {
+			$arguments['data']['password'] = $arguments['data']['new_password'];
+		}
 		// remove new password (because no such field in database)
 		if ( isset($arguments['data']['new_password']) ) unset($arguments['data']['new_password']);
 		// perform password hashing (when password specified)
-		$arguments['data']['password'] = Auth::hashPassword($arguments['data']['password']);
-		F::error(Auth::error(), $arguments['data']['password'] === false);
-		// [NOTE] no break ===> save by scaffold
+		if ( isset($arguments['data']['password']) ) {
+			$arguments['data']['password'] = Auth::hashPassword($arguments['data']['password']);
+			F::error(Auth::error(), $arguments['data']['password'] === false);
+		}
+		// no break, and continue scaffold process...
 
 
 	// crud operations
 	default:
+		// only show [new-password] field in edit form when password hash
+		$enterNewPassword = ( F::is('*.edit') and Auth::$hashPassword );
 		// config
 		$scaffold = array(
 			'beanType' => 'user',
@@ -46,28 +54,21 @@ switch ( $fusebox->action ) {
 			'listField' => array(
 				'id' => '60',
 				'role|fullname' => '20%',
-				'username|password' => '25%',
+				( $enterNewPassword ? 'username|new_password' : 'username|password' ) => '25%',
 				'email|tel' => '25%',
 			),
 			'fieldConfig' => array(
 				'id',
-				'role' => array('icon' => 'fa fa-tag small', 'default' => $_SESSION['userController__userRole'], 'readonly' => !Auth::userInRole('SUPER')),
-				'username' => array('icon' => 'fa fa-user small', 'placeholder' => true),
-F::is('*.edit') ? 'new_password' : 'password' => call_user_func(function(){
-	// no hash : simply show and edit password as normal field
-	if ( !Auth::$hashPassword ) {
-		return array('icon' => 'fa fa-key small', 'placeholder' => true);
-	// password hash : show message at listing
-	} elseif ( F::is('*.index,*.row') ) {
-		return array('format' => 'output', 'value' => '<span class="text-muted">[PASSWORD HASHED]</span>');
-	// password hash : show as empty field when edit
-	} else {
-		return array('icon' => 'fa fa-key small', 'placeholder' => F::is('*.edit') ? 'New Password' : true, 'value' => '');
-	}
-}),
+				'role' => array('icon' => 'fa fa-tag small', 'default' => $_SESSION['userController__userRole'], 'required' => true, 'readonly' => !Auth::userInRole('SUPER')),
+				'username' => array('icon' => 'fa fa-user small', 'placeholder' => true, 'required' => true),
+				'password' => array('icon' => 'fa fa-key small', 'placeholder' => true),
+				'new_password' => $enterNewPassword ? array('icon' => 'fa fa-key small', 'placeholder' => true) : false,
 				'fullname' => array('label' => 'Full Name', 'placeholder' => 'Full Name'),
 				'email' => array('icon' => 'fa fa-envelope small', 'placeholder' => true),
 				'tel' => array('icon' => 'fa fa-phone small', 'placeholder' => true)
+			),
+			'scriptPath' => array(
+				'row' => F::appPath('view/user/row.php'),
 			),
 			'writeLog' => class_exists('Log'),
 		);
